c7e93d8128421e734d1b679463f70089c8d84905
Santiago Marin Suarez <77743838+santiagoms2404@users.noreply.github.com>
2023-09-12 09:01:30 -0500

    React app running

--- server.js
+++ server.js
@@ -5,3 +5,17 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb+srv://santiagomarin:Pioj2404.@cluster0.qoltszy.mongodb.net/';
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
